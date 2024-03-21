33e094d9d8ed57edf59fcad83671977e6d25985c
Martin <gichurindungu@gmail.com>
2024-03-21 22:56:21 +0200

    insert into collections

--- tech_shop.py
+++ tech_shop.py
@@ -1,5 +1,8 @@
-from pymongo import Mongoclient
-client = Mongoclient()
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+import datetime
+client = MongoClient()
 
 db = client.tech_shop
 users_collection = db.users
@@ -11,6 +14,14 @@ def add_user(users):
     args: users (list): list of dictionaries each respresenting a single user
     returns: None'''
     users_collection.insert_many(users)
+
+user1 = {"name": "Alice", "email": "alice@example.com", "password": "securepass"}
+user2 = {"name": "Bob", "email": "bob@example.com", "password": "strongpass"}
+user3 = {"name": "Langdon", "email": "r.langon@havard.edu", "password": "Holy Grail" }
+user4 = {"name": "sofia", "email": "sofia@dcpj.fr", "password": "grand pier" }
+user5 = {"name": "Teabing", "email": "l.teabing@richness.com", "password": "The teacher" }
+user6 = {"name": "Aringarosa", "email": "b.aringarosa@vatican.com", "password": "Fear" }
+add_user(users=[user1, user2, user3, user4, user5, user6])
 def add_transactions(transactions):
     '''inserts multiple transactions into the transactions collection
     args: transactions: list of dictionaries, each representing transaction
@@ -21,4 +32,14 @@ def add_products(products):
     args: products: list of dictionaries, each representing a product
     returns: None'''
     products_collection.insert_many(products)
-    
+product1 = {"name": "Laptop", "description": "High-performance laptop", "price": 1000, "user_id": user1["_id"]}
+product2 = {"name": "Smartphone", "description": "Latest smartphone model", "price": 800, "user_id": user2["_id"]}
+product3 = {"name": "Hover", "description": "High-performance personal jet", "price": 5000, "user_id": user3["_id"]}
+product4 = {"name": "mickey mouse watch", "description": "unique golden watch", "price": 700, "user_id": user4["_id"]}
+product5 = {"name": "apple m1", "description": "M1 macbook", "price": 6000, "user_id": user5["_id"]}
+product6 = {"name": "soundcore q20", "description": "high-performance headphones", "price": 4500, "user_id": user6["_id"]}
+add_products([product1, product2, product3, product4, product5, product6])
+
+transaction = {"buyer_id": user1["_id"], "product_id": product1["_id"], "date": datetime.utcnow(), "quantity": 2}
+add_transactions([transaction])
+
